dc1b6a57e8ff788625f4b68b127fbd261b19c1c9
Anna Prosvetova <anna@prosvetova.me>
2021-03-10 04:43:26 +0300

    Update order.go

--- shopify/order.go
+++ shopify/order.go
@@ -33,6 +33,26 @@ type OrderRequest struct {
 	*Order `json:"order"`
 }
 
+func (s *OrderService) Get(ctx context.Context, orderID int64) (*Order, *http.Response, error) {
+	req, err := s.client.NewRequest(
+		"GET",
+		fmt.Sprintf("/admin/orders/%d.json", orderID),
+		nil,
+	)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	wrapper := &OrderRequest{&Order{}}
+	resp, err := s.client.Do(ctx, req, &wrapper)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return wrapper.Order, resp, nil
+}
+
+
 func (s *OrderService) AddNoteAttributes(ctx context.Context, orderID int64, attributes map[string]string) (*Order, *http.Response, error) {
 	var attrs []NoteAttribute
 	for k, v := range attributes {
